ac1e013c0bac286c17db84dd5a764131ffd492f7
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-11-03 13:53:24 -0800

    Decision Tree

---
+++ src/main/java/com/sjsu/healthcare/DecisionTree/UnknownDecisionException.java
@@ -0,0 +1,7 @@
+package com.sjsu.healthcare.DecisionTree;
+
+public class UnknownDecisionException extends Exception {
+    public UnknownDecisionException(String attribute, String decision) {
+        super();
+    }
+}
