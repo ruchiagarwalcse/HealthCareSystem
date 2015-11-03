4499030203f7856220f74cb0c1c34f8910e92808
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-11-02 16:59:08 -0800

    Decision Tree Implementation
    Data Cleanup, Model Data Generation & Decision Tree Implementation

---
+++ src/main/java/com/sjsu/healthcare/DecisionTree/Decisions.java
@@ -0,0 +1,38 @@
+package com.sjsu.healthcare.DecisionTree;
+
+import java.util.*;
+
+
+class Decisions {
+    private Map<String, Attribute> decisions;
+
+    public Decisions() {
+        decisions = new HashMap<String, Attribute>();
+    }
+
+    public Map<String, Attribute> getMap() {
+        return decisions;
+    }
+
+    public void put(String decision, Attribute attribute) {
+        decisions.put(decision, attribute);
+    }
+
+    public void clear() {
+        decisions.clear();
+    }
+
+    /**
+     * Returns the attribute based on the decision matching the provided value.
+     *
+     * Throws BadDecisionException if no decision matches.
+     */
+    public Attribute apply(String value) throws BadDecisionException {
+        Attribute result = decisions.get(value);
+
+        if ( result == null )
+            throw new BadDecisionException();
+
+        return result;
+    }
+}
